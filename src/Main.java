5264e63beb0693cbc34bbe318bf8da0d671c3ac0
Nguyen <huu.nguyen@hs-osnabrueck.de>
2022-12-01 14:17:39 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Slider slider = new Slider();
+        slider.mischen(slider.sliderArray);
+        slider.Spielfeld();
+        slider.spielen();
+
+    }
+}
---
+++ src/Slider.java
@@ -0,0 +1,47 @@
+import java.util.Random;
+
+public class Slider {
+    int sliderArray[][]={{0,1,2,3},{4,5,6,7},{8,9,10,11},{12,13,14,15}};
+    int zeilen=4;
+    int spalten=4;
+    int a;
+
+    public void mischen(int[][] a) {
+        /*Fisher–Yates Algorithmus*/
+        Random random = new Random();
+        for (int i = a.length - 1; i > 0; i--) {
+            for (int j = a[i].length - 1; j > 0; j--) {
+                int m = random.nextInt(i + 1);
+                int n = random.nextInt(j + 1);
+
+                int temp = a[i][j];
+                a[i][j] = a[m][n];
+                a[m][n] = temp;
+            }
+        }
+    }
+
+    public void Spielfeld(){
+     System.out.println("+--+--+--+--+--+--+");
+     System.out.println("| "+sliderArray[0][0]+" | "+sliderArray[0][1]+" | "+sliderArray[0][2]+" | "+sliderArray[0][3]+" | ");
+     System.out.println("+--+--+--+--+--+--+");
+     System.out.println("| "+sliderArray[1][0]+" | "+sliderArray[1][1]+" | "+sliderArray[1][2]+" | "+sliderArray[1][3]+" | ");
+     System.out.println("+--+--+--+--+--+--+");
+     System.out.println("| "+sliderArray[2][0]+" | "+sliderArray[2][1]+" | "+sliderArray[2][2]+" | "+sliderArray[2][3]+" | ");
+     System.out.println("+--+--+--+--+--+--+");
+     System.out.println("| "+sliderArray[3][0]+" | "+sliderArray[3][1]+" | "+sliderArray[3][2]+" | "+sliderArray[3][3]+" | ");
+     System.out.println("+--+--+--+--+--+--+");
+ }
+
+public void spielen(){
+        EinUndAusgabe einlesen = new EinUndAusgabe();
+    System.out.println("Zeile: ");
+    int zeile= einlesen.leseInteger();
+    System.out.println("Spalte: ");
+    int spalte= einlesen.leseInteger();
+
+    if (zeile==sliderArray[zeile][spalte]){
+
+    }
+    }
+}
